4c77209a73fa5e50d98ac6eeb2c53336d3f089e5
Tom Coyle <guitardude0825@yahoo.com>
2021-06-11 14:31:57 -0400

    Initial Change

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region = var.region
+  profile = "pop"
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/coyle/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
